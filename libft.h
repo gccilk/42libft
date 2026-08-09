19e7cb5a2673c2e1d67952358a256712a0be2cd6
Ilayda Kaptanoglu <ilkaptan@k1m19s03.42istanbul.com.tr>
2026-08-09 20:31:57 +0300

    libft update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ilkaptan <ilkaptan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 17:52:59 by ilkaptan          #+#    #+#             */
-/*   Updated: 2026/08/09 19:04:52 by ilkaptan         ###   ########.fr       */
+/*   Updated: 2026/08/09 20:31:38 by ilkaptan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,10 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
